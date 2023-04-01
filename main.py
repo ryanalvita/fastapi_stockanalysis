d20a155b6040d2cbaad38455ff2e144e984b1f58
ryanalvita <ryanalvita@yahoo.com>
2023-04-01 03:35:59 +0200

    Remove cors

--- main.py
+++ main.py
@@ -16,16 +16,6 @@ app = FastAPI(
     },
 )
 
-origins = ["*"]
-
-app.add_middleware(
-    CORS,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 app.include_router(router.router)
 
 if __name__ == "__main__":
